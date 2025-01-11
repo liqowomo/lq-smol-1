a0804fc72152bedf6bd87f9bf087a91045ca9f94
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-01-11 04:15:17 +0000

    🫦

--- WO/agen1/panty.py
+++ WO/agen1/panty.py
@@ -4,15 +4,14 @@ from rich import print as rprint
 
 
 def main():
-    run_autogen()
-
-
-# Executing the autogen
-def run_autogen():
     rprint("""
 Executing autogen stdudio ... Ctrl + C to exit
 [+] uv run autogen ui
 """)
+
+
+# Executing the autogen
+def run_autogen():
     # This function throws and error
     subprocess.run("uv", "run", "autogen", "ui")
 
