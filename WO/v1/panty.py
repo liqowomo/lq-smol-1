a7e14cd9f1a2cb7bacaef469900e9daaa6ebe067
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-01-11 02:55:58 +0000

    🫦

--- WO/v1/panty.py
+++ WO/v1/panty.py
@@ -1,13 +1,16 @@
 # Python file to execute marimo basically
 import subprocess
 from rich import print as rprint
+from rich.traceback import install
+install(show_locals=True)
 
 def main():
-    rprint("""
-    [magenta]Executing file - marimopanty.py""")
+    rprint("""[magenta]
+Executing file - marimopanty.py[/magenta]""")
+    run_marimo()
 
 def run_marimo():
-    subprocess.run("uv", "run", "marimo", "marimopanty.py")
+    subprocess.run("uv", "run", "marimo", "edit" ,"marimopanty.py")
 
 if __name__ == "__main__":
     main()
