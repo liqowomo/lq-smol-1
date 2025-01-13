b9bbfda91a952eebf136b2bcdc688a84f19e0b3a
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-01-13 16:46:26 +0000

    🫦

--- WO/CREWAI/c2/panty.py
+++ WO/CREWAI/c2/panty.py
@@ -1,6 +1,12 @@
 from utils.utils import label1
 
 
+from pus1.src.pus1.main import run
+
+from rich.traceback import install
+from rich import print as rprint
+
+
 def main():
     label1("Running pus1 Crew")
 
