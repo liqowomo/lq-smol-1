627ef02ecb8967275ce3c60aba3004fe079b0f95
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-01-13 17:09:58 +0000

    🫦

--- WO/CREWAI/c2/panty.py
+++ WO/CREWAI/c2/panty.py
@@ -1,15 +1,28 @@
 from utils.utils import label1
 from pu1.src.pu1.main import pu1run
+from pu2.src.pu2.main import pu2run
 
 from rich.traceback import install
 from rich import print as rprint
 
 
 def main():
+    pu2runz()
+
+
+def pu1runz():
     label1("""
 Running pus1 Crew - from pu1.src.pu1.main import run
 function pu1run""")
     pu1run()
+
+
+def pu2runz():
+    label1("""
+Running pus1 Crew - from pu2.src.pu2.main import pu2run
+Second crew called pu2 being run from inside the same env
+""")
+    pu2run()
 
 
 if __name__ == "__main__":
--- WO/CREWAI/c2/pu2/src/pu2/main.py
+++ WO/CREWAI/c2/pu2/src/pu2/main.py
@@ -2,7 +2,7 @@
 import sys
 import warnings
 
-from pu2.crew import Pu2
+from .crew import Pu2
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
@@ -11,12 +11,13 @@ warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 # Replace with inputs you want to test with, it will automatically
 # interpolate any tasks and agents information
 
-def run():
+
+def pu2run():
     """
     Run the crew.
     """
     inputs = {
-        'topic': 'AI LLMs'
+        "topic": "Describe Offensive penetration techniques in ethereum based smart contracts"
     }
     Pu2().crew().kickoff(inputs=inputs)
 
@@ -25,14 +26,15 @@ def train():
     """
     Train the crew for a given number of iterations.
     """
-    inputs = {
-        "topic": "AI LLMs"
-    }
+    inputs = {"topic": "AI LLMs"}
     try:
-        Pu2().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+        Pu2().crew().train(
+            n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs
+        )
 
     except Exception as e:
         raise Exception(f"An error occurred while training the crew: {e}")
+
 
 def replay():
     """
@@ -44,15 +46,16 @@ def replay():
     except Exception as e:
         raise Exception(f"An error occurred while replaying the crew: {e}")
 
+
 def test():
     """
     Test the crew execution and returns the results.
     """
-    inputs = {
-        "topic": "AI LLMs"
-    }
+    inputs = {"topic": "AI LLMs"}
     try:
-        Pu2().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+        Pu2().crew().test(
+            n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs
+        )
 
     except Exception as e:
         raise Exception(f"An error occurred while replaying the crew: {e}")
