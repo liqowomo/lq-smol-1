f8e3630de76464d6734092ca998d93f4e78220f6
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-01-14 17:19:15 +0000

    🫦

--- WO/CREWAI/pro1/panty.py
+++ WO/CREWAI/pro1/panty.py
@@ -1,7 +1,7 @@
 # Standard imports
 from ut.utils import label1
 from crz.p1.src.p1.main import p1_run
-
+from crz.poem_flow.src.poem_flow.main import kickoff, plot
 
 # --- Imports from rich for better errors
 from rich.traceback import install
@@ -11,14 +11,19 @@ install(show_locals=False)
 
 # --- Main Function ---
 def main():
-    label1("Exdcuting...crz/C1 - Crew 1")
-    P1RUN()
+    powem_flowRUN()
 
 
 # --- Functions being called from the crews -
 def P1RUN():
     label1("crz/P1 Run")
     p1_run()
+
+
+def powem_flowRUN():
+    label1("crz/poemflow/kickoff and plot")
+    kickoff()
+    plot()
 
 
 if __name__ == "__main__":
