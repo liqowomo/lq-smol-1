3163d4768a933d6850b02e7420552cac9cef60df
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-01-16 17:08:23 +0000

    🫦

--- TUTO/gra1/src/g1.py
+++ TUTO/gra1/src/g1.py
@@ -8,5 +8,11 @@ def App1():
     def greet(name, intensity):
         return "Hello " + name + ", " + int(intensity)
 
-    demo = gr.Interface(fn=greet, inputs=["text", "slider"], outputs="text")
+    demo = gr.Interface(
+        fn=greet,
+        inputs=["text", "slider"],
+        outputs="text",
+        title="SmellPanty",
+        description="Sucking and Fucking",
+    )
     demo.launch()
