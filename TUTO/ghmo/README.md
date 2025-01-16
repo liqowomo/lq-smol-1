b618426580f5982cf3682cc7b7131969fc4e3d44
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-01-16 03:19:40 +0000

    🫦

---
+++ TUTO/ghmo/README.md
@@ -0,0 +1,8 @@
+
+1. [GHMO](#ghmo)
+
+
+# GHMO
+
+1. Testing out GHMO visa GHPATS 
+2. Using async tang
---
+++ TUTO/ghmo/panty.py
@@ -0,0 +1,13 @@
+# Main Entry Point here
+from ut.utils import label1
+from rich.traceback import install
+
+install(show_locals=True)
+
+
+def main():
+    label1("Panty.py")
+
+
+if __name__ == "__main__":
+    main()
