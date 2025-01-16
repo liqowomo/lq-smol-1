c7f9b0b6a983b9db8920851adba7bdd76589cb6c
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-01-16 01:31:25 +0000

    🫦

--- WO/CREWAI/pro2/crz/mimu/src/mimu/main.py
+++ WO/CREWAI/pro2/crz/mimu/src/mimu/main.py
@@ -1,42 +1,17 @@
 #!/usr/bin/env python
-from random import randint
-
 from pydantic import BaseModel
-
 from crewai.flow.flow import Flow, listen, start
 
-from mimu.crews.poem_crew.poem_crew import PoemCrew
+
+class MimuFlowState(BaseModel):
+    transcription: str = ""
+    mimu_minutes: str = ""
 
 
-class PoemState(BaseModel):
-    sentence_count: int = 1
-    poem: str = ""
-
-
-class PoemFlow(Flow[PoemState]):
-
+class MimuFlow(Flow[PoemState]):
     @start()
     def generate_sentence_count(self):
         print("Generating sentence count")
-        self.state.sentence_count = randint(1, 5)
-
-    @listen(generate_sentence_count)
-    def generate_poem(self):
-        print("Generating poem")
-        result = (
-            PoemCrew()
-            .crew()
-            .kickoff(inputs={"sentence_count": self.state.sentence_count})
-        )
-
-        print("Poem generated", result.raw)
-        self.state.poem = result.raw
-
-    @listen(generate_poem)
-    def save_poem(self):
-        print("Saving poem")
-        with open("poem.txt", "w") as f:
-            f.write(self.state.poem)
 
 
 def kickoff():
