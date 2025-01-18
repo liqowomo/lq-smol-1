3a44799b9082745a1e608e478c7ef2512bd6ed2a
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-01-18 12:29:16 +0000

    🫦

---
+++ TUTO/aigradio1/README.md
@@ -0,0 +1,8 @@
+
+1. [aigradio1](#aigradio1)
+
+
+# aigradio1
+
+1. Testing - https://github.com/AK391/ai-gradio
+2. Simple CHAT UI Interface , mch more simplert than lobechat
---
+++ TUTO/aigradio1/panty.py
@@ -0,0 +1,12 @@
+from ut.utils import label1
+from rich.traceback import install
+
+install(show_locals=True)
+
+
+def main():
+    print("Hello from aigradio1!")
+
+
+if __name__ == "__main__":
+    main()
