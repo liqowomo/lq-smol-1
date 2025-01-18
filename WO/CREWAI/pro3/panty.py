6b84937276342b90a7e98ea1a68a36ef0509dbf0
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-01-18 06:23:49 +0000

    🫦

--- WO/CREWAI/pro3/panty.py
+++ WO/CREWAI/pro3/panty.py
@@ -1,8 +1,15 @@
 # Crew actual entry point
+import agentops
+from ut.utils import label1
+
+from rich.traceback import install
+
+agentops.init()  # Required fro agentops
+install(show_locals=False)  # Required for prettier errors
 
 
 def main():
-    print("Hello from pro3!")
+    label1("Flowza")
 
 
 if __name__ == "__main__":
