a14cce4c0ac7268a061fd06e70e8a985157b68f5
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-01-18 06:47:20 +0000

    🫦

--- WO/CREWAI/pro3/crz/mimu/src/mimu/main.py
+++ WO/CREWAI/pro3/crz/mimu/src/mimu/main.py
@@ -3,44 +3,20 @@ from pydantic import BaseModel
 from crewai.flow.flow import Flow, listen, start
 
 
-class PoemState(BaseModel):
-    sentence_count: int = 1
-    poem: str = ""
+class MeetingMintuesState(BaseModel):
+    transcript: str = ""
+    meeting_minutes: str = ""
 
 
-class PoemFlow(Flow[PoemState]):
+class MeetingMintuesFlow(Flow[MeetingMintuesState]):
     @start()
     def generate_sentence_count(self):
         print("Generating sentence count")
-        self.state.sentence_count = randint(1, 5)
-
-    @listen(generate_sentence_count)
-    def generate_poem(self):
-        print("Generating poem")
-        result = (
-            PoemCrew()
-            .crew()
-            .kickoff(inputs={"sentence_count": self.state.sentence_count})
-        )
-
-        print("Poem generated", result.raw)
-        self.state.poem = result.raw
-
-    @listen(generate_poem)
-    def save_poem(self):
-        print("Saving poem")
-        with open("poem.txt", "w") as f:
-            f.write(self.state.poem)
 
 
 def kickoff():
-    poem_flow = PoemFlow()
-    poem_flow.kickoff()
-
-
-def plot():
-    poem_flow = PoemFlow()
-    poem_flow.plot()
+    meeting_minutes_flow = MeetingMintuesFlow()
+    meeting_minutes_flow.kickoff()
 
 
 if __name__ == "__main__":
